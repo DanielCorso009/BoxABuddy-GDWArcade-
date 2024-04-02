12a8d7fb5b83f21b33cdfeb943cb3fcd413fc530
DanielCorso009 <dcorso1@student.fitchburgstate.edu>
2024-04-02 12:06:30 -0400

    finished Game

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeookLeft : StateMachineBehaviour
+public class Attack: StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     public GameObject hitbox;
